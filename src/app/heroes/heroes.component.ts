4e067f9444b9ad348ed3abb982f736673dd8f469
capi <leo@malgrange.fr>
2019-02-05 21:35:26 +0100

    toh step 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../model/hero';
+import { HEROES } from '../model/mock-heroes';
 
 @Component({
   selector: 'toh-heroes',
@@ -8,14 +9,15 @@ import { Hero } from '../model/hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
