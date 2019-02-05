097a3b8df43c51fa406e094f5fab1d4a7fdad996
capi <leo@malgrange.fr>
2019-02-05 08:53:41 +0100

    add heroes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'toh-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
